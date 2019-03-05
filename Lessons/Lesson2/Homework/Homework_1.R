8f8c62a1d2f37ebe6955954b37bde90943560fd4
Sedmokraska <47779370+Sedmokraska@users.noreply.github.com>
2019-03-05 07:47:16 +0100

    Update Homework_1.R

--- Lessons/Lesson2/Homework/Homework_1.R
+++ Lessons/Lesson2/Homework/Homework_1.R
@@ -3,7 +3,33 @@
 # __Commit__ it to your repository into `Lessons/Lesson2/Homework`.
 
 ## Code
-# 
+# #instalujeme balicek
+install.packages("dplyr")
+library(dplyr)
+
+#nacitanie dat
+setwd("H:/Aktuarstvo/GeneralInsurance_Class/Data")
+dt_KPI <- read.csv("lesson2_KPI.csv")
+
+#zobrazenie prvych par dat
+dt_KPI %>% head
+
+#zgrupneme data podla unit, pre kazdu unit vyratame profit, nascitame profity a nasledne to zoradime od najvacsieho profitu
+dt_KPI %>% group_by(Unit) %>% mutate(profit=Premium-Expenses-Losses) %>% summarize(profit = sum(profit, na.rm = TRUE)) %>% arrange(desc(profit))
+
+#zistili sme, ze najviac profitovy je Unit7, preto s nim dalej budeme pracovat
+#vyberieme data prinaleziace unit7, pre kazde spravime profit, zgrupneme to podla rokov, zosumarizujeme profity pre jednotlive roky a zoradime od najmensieho profitu
+dt_KPI %>% filter(Unit == "Unit7")  %>% mutate(profit=Premium-Expenses-Losses) %>% group_by(Year) %>% summarize(profit=sum(profit, na.rm = TRUE)) %>% arrange(profit)
+
+#vyjde nam rok 2014
+
+#instalujeme druhy balicek
+install.packages("ggplot2")
+library(ggplot2)
+
+#vykreslenie rocnych profitov unit7
+dt_KPI %>% filter(Unit == "Unit7")  %>% mutate(profit=Premium-Expenses-Losses) %>% group_by(Year) %>% summarize(profit=sum(profit, na.rm = TRUE)) %>% arrange(profit) %>% ggplot(aes(x = Year, y = profit)) + geom_col()
+
 # 
 # 
 # 
@@ -29,6 +55,12 @@
 
 
 # Your Explanation about analysis:
-# 
-# 
-# 
+# nainstalovali sme balicek dplyr
+# nacitali sme data
+# zobrazili sme prvych par dat
+#zgrupli sme data podla unit, pre kazdu unit vyratali profit, nascitali profity a nasledne to zoradili od najvacsieho profitu
+#zistili sme, ze najviac profitovy je Unit7, preto sme s nim dalej pracovali
+#vybrali sme si data prinaleziace unit7, pre kazde spravili profit, zgrupli to podla rokov, zosumarizovali profity pre jednotlive roky a zoradimli od najmensieho profitu
+#zistili sme ze najmenej profitovym bol rok 2014
+#nainstalovali sme balicek ggplot2
+#graficky znazornili profity pre unit7 za kazdy rok
